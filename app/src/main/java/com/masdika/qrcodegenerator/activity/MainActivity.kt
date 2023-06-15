c8b3ff9c1b054d3f734bc8981c6bac0a528d8c0c
Masdikaa <masdikailhanmansiz@gmail.com>
2023-06-15 16:44:44 +0700

    Build QR code image Bitmap in MainActivity

--- app/src/main/java/com/masdika/qrcodegenerator/activity/MainActivity.kt
+++ app/src/main/java/com/masdika/qrcodegenerator/activity/MainActivity.kt
@@ -1,21 +1,55 @@
 package com.masdika.qrcodegenerator.activity
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+import com.journeyapps.barcodescanner.BarcodeEncoder
 import com.masdika.qrcodegenerator.R
 import com.masdika.qrcodegenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         window.statusBarColor = ContextCompat.getColor(this, R.color.white)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var textInput: String
+
+        binding.btnGenerate.setOnClickListener {
+            textInput = binding.inputText.text.toString()
+            val writer: MultiFormatWriter = MultiFormatWriter()
+
+            if (textInput.isEmpty()) {
+                binding.inputText.error = "Field belum di isi!"
+                binding.inputText.requestFocus()
+            } else {
+
+                try {
+                    val bitMatrix: BitMatrix =
+                        writer.encode(textInput, BarcodeFormat.QR_CODE, 1000, 1000)
+                    val barcodeEncoder: BarcodeEncoder = BarcodeEncoder()
+                    val bitmap: Bitmap = barcodeEncoder.createBitmap(bitMatrix)
+
+                    binding.imageQrResult.setImageBitmap(bitmap)
+
+                } catch (e: WriterException) {
+                    Log.e("Error", e.toString())
+                }
+            }
+        }
 
     }
 }
