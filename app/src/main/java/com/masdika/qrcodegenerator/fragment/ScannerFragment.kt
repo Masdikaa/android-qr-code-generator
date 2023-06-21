fe5bc9b31f4d2b0870fe97741dac81d325f74abe
Masdikaa <masdikailhanmansiz@gmail.com>
2023-06-21 10:27:56 +0700

    Implement documentation procedure, successfully used to scan qr code

--- app/src/main/java/com/masdika/qrcodegenerator/fragment/ScannerFragment.kt
+++ app/src/main/java/com/masdika/qrcodegenerator/fragment/ScannerFragment.kt
@@ -1,21 +1,111 @@
 package com.masdika.qrcodegenerator.fragment
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.masdika.qrcodegenerator.R
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import com.masdika.qrcodegenerator.databinding.FragmentScannerBinding
 
 class ScannerFragment : Fragment() {
+
+    private var _binding: FragmentScannerBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var codeScanner: CodeScanner
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scanner, container, false)
+        _binding = FragmentScannerBinding.inflate(inflater, container, false)
+
+        codeScanner()
+        setPermission()
+
+        return binding.root
     }
 
+    private fun setPermission() {
+        val permission =
+            ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
 
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeReq()
+        }
+    }
+
+    private fun makeReq() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(android.Manifest.permission.CAMERA),
+            101
+        )
+    }
+
+    @Suppress("DEPRECATION")
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            101 -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(context, "Camera permission is required", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+    }
+
+    private fun codeScanner() {
+        codeScanner = CodeScanner(requireContext(), binding.scannerView)
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                activity?.runOnUiThread {
+                    binding.resultField.text = it.text
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                activity?.runOnUiThread {
+                    Toast.makeText(context, "${it.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            binding.scannerView.setOnClickListener {
+                startPreview()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        codeScanner.startPreview()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
 }
